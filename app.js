4b352f4811886b422c7ca5a8b521700c108afb58
micahmura21 <Mickmura21@gmail.com>
2017-06-27 16:40:21 -1000

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,47 @@
 function encodeMsg (message){
-	var encryptedMsg
+	var newMessage = "";
+	for (var i = 0; i < message.length; i++){
+ 		if (message[i] === "a"){
+ 			newMessage += 4;
+ 		}
+ 		else if (message[i] === "e"){
+ 			newMessage += 3;
+ 		}
+ 		else if (message[i] === "i"){
+ 			newMessage += 1;
+ 		}
+ 		else if (message[i] === "o"){
+ 			newMessage += 0;
+ 		}
+ 		else{
+ 			newMessage += message[i];
+ 		}	
+ 	}
+ console.log(newMessage) 
+}
+encodeMsg('Javascript is Amazing')
 
-	for (var i = message.length - 1; i >= 0; i--) {
-	message[i]
+function decodeMsg (message){
+	var newMessage = "";
+	for (var i = 0; i < message.length; i++){
+ 		if (message[i] === "4"){
+ 			newMessage +='a' ;
+ 		}
+ 		else if (message[i] === "3"){
+ 			newMessage += 'e' ;
+ 		}
+ 		else if (message[i] === "1"){
+ 			newMessage += 'i';
+ 		}
+ 		else if (message[i] === "0"){
+ 			newMessage += 'o';
+ 		}
+ 		else{
+ 			newMessage += message[i];
+ 		}	
+ 	}
+ console.log(newMessage) 
+
+
 }
-console.log (message)
-}
+decodeMsg('J4v4scr1pt 1s Am4z1ng')
